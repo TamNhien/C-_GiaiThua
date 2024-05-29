6e184d6429be8624fcbd1eb1d653e25a9d25db08
Canh Thien <canhthien91@gmail.com>
2024-05-29 21:53:19 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap so can tinh giai thua: ");
+        int sonhap = int.Parse(Console.ReadLine());
+        long giaithua = 1;
+
+        for (int i = 1; i <= sonhap; i++)
+        {
+            giaithua *= i;
+        }
+        Console.WriteLine($"Giai thua cua {sonhap} la: {giaithua}");
+    }
+}
